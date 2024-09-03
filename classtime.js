908d45dbdd30658855c54b96d3cbc25c0e5df855
Brandon Yang <143979814+WhisperRock@users.noreply.github.com>
2024-09-02 20:30:11 -0400

    Update classtime.js
    Regular

--- classtime.js
+++ classtime.js
@@ -1,8 +1,8 @@
 let pageTitle = "";
 let scheduleMap = new Map();
 
-let mod = true;
-let showTimeline = false;
+let mod = false;
+let showTimeline = true;
 /*
 If you are reading this, why hello there! 
 I am writing this because I have to remind myself what to do every time there's a modified schedule
